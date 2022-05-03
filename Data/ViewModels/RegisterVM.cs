32dd71258380d216d92161924027ef0c37bf895c
Christian Fonkeng <echristianfonkeng@gmail.com>
2022-05-04 00:44:31 +0100

    Login and Logout functionalities added

---
+++ Data/ViewModels/RegisterVM.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewPhoneShop2.Data.ViewModels
+{
+    public class RegisterVM
+    {
+        [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full name is required")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Email Address is required")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [Required(ErrorMessage = "Confirm password required")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
